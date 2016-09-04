0c70e08f7f08879526148580a794e4501b5f2a69
diegofk26 <diegofk26@gmail.com>
2016-09-03 21:54:41 -0300

    Se agrego la validacion de la resolucion -r. Closes #4
    Se realizaron los cambios para soportar inicializar los parametros
    utilizando los argumentos y ademas se agrego la validacion de la
    resolucion.
    
    Funciones agregadas:
    bool isAValidNumberInAString(char* numberRepresentedAsAString);
    void invalidParameter(char* errorMSG);
    void validateResolutionArgument(char* argument, int* width, int* height);
    void inputValidationAndInitialization(int argc, char* argv[], SetSpace** setSpace,GraphicSettings** graphicSettings);

--- src/JuliaSet.c
+++ src/JuliaSet.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define C_REAL_PART -0.4
 #define C_IMAGINARY_PART 0.6
@@ -10,6 +15,12 @@
 #define SPACE_HEIGHT 10
 #define SPACE_WIDTH 10
 #define BRIGHT_BOOST 3
+#define DEFAULT_FILE "file.pgm"
+
+#define RESOLUTION 'r'
+#define ERROR_INVALID_RESOLUTION "fatal: invalid resolution specification."
+
+
 
 typedef struct ComplexNumber{
 	double realPart;
@@ -29,44 +40,118 @@ typedef struct Pixel{
 } Pixel;
 
 typedef struct GraphicSettings{
+	int widthResolution;
+	int heightResolution;
 	Pixel *pixelGrid;
 	FILE *fileOuptut;
 } GraphicSettings;
 
-SetSpace *initializeSpace();
-GraphicSettings *initializeGraphicSettings();
-void findJuliaSet (SetSpace *aSpace, Pixel *pixels);
-void printOutput(FILE *anOutput);
-FILE *getFileOutput(GraphicSettings *aGraphicSettings);
+
+
+
+
+
+
+
+
+
+
+bool isAValidNumberInAString(char* numberRepresentedAsAString);
+void invalidParameter(char* errorMSG);
+void validateResolutionArgument(char* argument, int* width, int* height);
+void inputValidationAndInitialization(int argc, char* argv[], SetSpace** setSpace,GraphicSettings** graphicSettings);
+SetSpace *initializeDefaultSpace();
+GraphicSettings *initializeGraphicSettings(int widtRes, int heightRes,char* file);
+void printOutput(GraphicSettings *aGraphicSettings);
+FILE *getFileOutput(char *aFileName);
 int getResolution(char *aType, char *anImageResolution);
-ComplexNumber getComplexNumber(char *aComplexNumberStringFormat);
-
-int main(int argc, char *argv[]){												//por ahora argv tiene el orden = 1. archivo 2. resolucion 
-																				//3. offset 4. C 5. width 6. height  				
-	SetSpace *aSpace = initializeSpace();
-	GraphicSettings *theGraphicSettings = initializeGraphicSettings();
-	
-
-	findJuliaSet(aSpace, (*theGraphicSettings).pixelGrid);
-	printOutput(theGraphicSettings);
+double absoluteValue(ComplexNumber z);
+unsigned char calculateEscapeVelocity(ComplexNumber z, ComplexNumber c);
+void calculatePixelsPosition(SetSpace *aSpace, Pixel *pixels, int resolutionWidth, int resolutionHeight);
+void findJuliaSet (SetSpace *aSpace, Pixel *pixels, int width,int height);
 
 
-	free(aSpace);
-	aSpace = NULL;
-	free(theGraphicSettings);
-	theGraphicSettings = NULL;
 
-	return 0;
+
+
+
+bool isAValidNumberInAString(char* numberRepresentedAsAString){
+  if(strlen(numberRepresentedAsAString) == 0)
+    return false;
+  for(int i = 0; i < strlen(numberRepresentedAsAString); i++){
+    if(!isdigit(numberRepresentedAsAString[i]))
+            return false;
+  }
+  return true;
 }
 
 
-SetSpace *initializeSpace(){
+
+void invalidParameter(char* errorMSG){
+  printf("%s\n",errorMSG);
+  exit(EXIT_FAILURE);
+}
+
+
+
+void validateResolutionArgument(char* argument, int* width, int* height){
+  //Resolution must be NUMBERxNUMBER ( 100x100 )
+  if(strlen(argument) == 0)
+    invalidParameter(ERROR_INVALID_RESOLUTION);
+  char delimiter[1] = "x";
+  char* firstNumberString = strsep(&argument,delimiter);
+  char* secondNumberString = strsep(&argument,delimiter);
+  if(!firstNumberString || !secondNumberString){
+    invalidParameter(ERROR_INVALID_RESOLUTION);
+  }
+
+  if(!isAValidNumberInAString(firstNumberString) ||
+  !isAValidNumberInAString(secondNumberString)){
+    invalidParameter(ERROR_INVALID_RESOLUTION);
+  }
+
+	*width = atoi(firstNumberString);
+	*height = atoi(secondNumberString);
+}
+
+
+
+void inputValidationAndInitialization(int argc, char* argv[], SetSpace** setSpace,GraphicSettings** graphicSettings){
+
+	int widthResolution = WIDTH;
+	int heightResolution = HEIGHT;
+
+	char file[] = DEFAULT_FILE;
+	*setSpace = initializeDefaultSpace();
+
+	int c;
+	opterr = 0;
+	while ((c = getopt (argc, argv, "r:")) != -1)
+		switch (c)
+			{
+			case RESOLUTION:
+				validateResolutionArgument(optarg,&widthResolution,&heightResolution);
+				break;
+			case '?'://ALGUN PARAMETRO NO TIENE PAR
+				invalidParameter("fatal: some of the input parameters were not specified");
+				break;
+			default:
+				abort ();
+			}
+
+	*graphicSettings = initializeGraphicSettings(widthResolution,heightResolution,file);
+
+}
+
+
+
+SetSpace *initializeDefaultSpace(){
 
 	SetSpace *aNewSpace = malloc(sizeof(SetSpace));
 
 	(*aNewSpace).offset.realPart = OFFSET_REAL_PART;
 	(*aNewSpace).offset.imaginaryPart = OFFSET_IMAGINARY_PART;
-						
+
 	(*aNewSpace).constantC.realPart = C_REAL_PART;
 	(*aNewSpace).constantC.imaginaryPart = C_IMAGINARY_PART;
 
@@ -77,60 +162,71 @@ SetSpace *initializeSpace(){
 }
 
 
-GraphicSettings *initializeGraphicSettings(){
+
+GraphicSettings *initializeGraphicSettings(int widtRes, int heightRes,char* file){
 
 	GraphicSettings * aNewGraphicSettings = malloc(sizeof(GraphicSettings));
+	(*aNewGraphicSettings).widthResolution = widtRes;
+	(*aNewGraphicSettings).heightResolution = heightRes;
 
-	int widthResolution = WIDTH; 	//getResolution("width", argv[1]);
-	int heightResolution = HEIGHT; 	//getResolution("height", argv[1]);
-
-	(*aNewGraphicSettings).pixelGrid = malloc(sizeof(Pixel) * widthResolution * heightResolution);
-	for(int i = 0; i < heightResolution; i++){
-		for(int j = 0; j < widthResolution; j++){
+	(*aNewGraphicSettings).pixelGrid = malloc(sizeof(Pixel) * (*aNewGraphicSettings).widthResolution * (*aNewGraphicSettings).heightResolution);
+	for(int i = 0; i < (*aNewGraphicSettings).heightResolution; i++){
+		for(int j = 0; j < (*aNewGraphicSettings).widthResolution; j++){
 			Pixel aNewPixel;
 			aNewPixel.position.realPart = i;
 			aNewPixel.position.imaginaryPart = j;
-			aNewPixel.bright = 0; 
-			(*aNewGraphicSettings).pixelGrid[i * widthResolution + j] = aNewPixel;
+			aNewPixel.bright = 0;
+			(*aNewGraphicSettings).pixelGrid[i * (*aNewGraphicSettings).widthResolution + j] = aNewPixel;
 		}
 	}
-
-	(*aNewGraphicSettings).fileOuptut = getFileOutput("asd");
-
+	(*aNewGraphicSettings).fileOuptut = getFileOutput(file);
 	return aNewGraphicSettings;
 }
 
+
+
 void printOutput(GraphicSettings *aGraphicSettings){
 
-	fprintf((*aGraphicSettings).fileOuptut, "P5\n%u %u 255\n", WIDTH, HEIGHT);
-	for(int i = 0; i < HEIGHT; i++){
-		for(int j = 0; j < WIDTH; j++){
-			char pixelToWrite = (*aGraphicSettings).pixelGrid[i * WIDTH + j].bright * BRIGHT_BOOST;
+	fprintf((*aGraphicSettings).fileOuptut, "P5\n%u %u 255\n", (*aGraphicSettings).widthResolution, (*aGraphicSettings).heightResolution);
+	for(int i = 0; i < (*aGraphicSettings).heightResolution; i++){
+		for(int j = 0; j < (*aGraphicSettings).widthResolution; j++){
+			char pixelToWrite = (*aGraphicSettings).pixelGrid[i * (*aGraphicSettings).widthResolution + j].bright * BRIGHT_BOOST;
 			fputc(pixelToWrite, (*aGraphicSettings).fileOuptut);
 		}
 	}
 	fclose((*aGraphicSettings).fileOuptut);
 }
 
-FILE *getFileOutput(char *aFileName[]){
-	FILE *outputFile = fopen("Prueba.pgm", "wb");
+
+
+FILE *getFileOutput(char *aFileName){
+	FILE *outputFile = fopen(aFileName, "wb");
 	if (outputFile == NULL)
 		perror("Error");
+	return outputFile;
 }
+
+
 
 int getResolution(char *aType, char *anImageResolution){
 	return 1920;
 }
 
+
+
 /*ComplexNumber getComplexNumber(char *aComplexNumberStringFormat){
 
 }*/
+
+
 
 double absoluteValue(ComplexNumber z){
 
 	double squareSum = (z.realPart * z.realPart) + (z.imaginaryPart * z.imaginaryPart);
 	return (double) sqrt(squareSum);
 }
+
+
 
 unsigned char calculateEscapeVelocity(ComplexNumber z, ComplexNumber c){
 	int i;
@@ -146,27 +242,29 @@ unsigned char calculateEscapeVelocity(ComplexNumber z, ComplexNumber c){
 	return i;
 }
 
-void calculatePixelsPosition(SetSpace *aSpace, Pixel *pixels){
-	double scaleFactorRealAxis = (*aSpace).width / WIDTH;
-	double scaleFactorImaginaryAxis = (*aSpace).height / HEIGHT;
+
+
+void calculatePixelsPosition(SetSpace *aSpace, Pixel *pixels, int resolutionWidth, int resolutionHeight){
+	double scaleFactorRealAxis = (*aSpace).width / resolutionWidth;
+	double scaleFactorImaginaryAxis = (*aSpace).height / resolutionHeight;
 
 	double middlePointRealAxis = scaleFactorRealAxis / 2;
 	double middlePointImaginaryAxis = scaleFactorImaginaryAxis / 2;
 
 	double startPointRealAxis = (*aSpace).offset.realPart - (*aSpace).width/2 + middlePointRealAxis;
-	double startPointImaginaryAxis = (*aSpace).offset.imaginaryPart - (*aSpace).height/2 + middlePointImaginaryAxis;	
+	double startPointImaginaryAxis = (*aSpace).offset.imaginaryPart - (*aSpace).height/2 + middlePointImaginaryAxis;
 
 	double positionIncrementRealAxis = startPointRealAxis;
 	double positionIncrementImaginaryAxis = startPointImaginaryAxis;
-	for(int i = 0; i < HEIGHT; i++){
-		for(int j = 0; j < WIDTH; j++){
+	for(int i = 0; i < resolutionHeight; i++){
+		for(int j = 0; j < resolutionWidth; j++){
 
 			if(i == 0 && j == 0){
-				pixels[i * WIDTH + j].position.realPart = startPointRealAxis;
-				pixels[i * WIDTH + j].position.imaginaryPart = startPointImaginaryAxis;
+				pixels[i * resolutionWidth + j].position.realPart = startPointRealAxis;
+				pixels[i * resolutionWidth + j].position.imaginaryPart = startPointImaginaryAxis;
 			} else {
-				pixels[i * WIDTH + j].position.realPart = positionIncrementRealAxis;;
-				pixels[i * WIDTH + j].position.imaginaryPart = positionIncrementImaginaryAxis;				
+				pixels[i * resolutionWidth + j].position.realPart = positionIncrementRealAxis;;
+				pixels[i * resolutionWidth + j].position.imaginaryPart = positionIncrementImaginaryAxis;
 			}
 
 			positionIncrementRealAxis += middlePointRealAxis;
@@ -176,13 +274,31 @@ void calculatePixelsPosition(SetSpace *aSpace, Pixel *pixels){
 	}
 }
 
-void findJuliaSet (SetSpace *aSpace, Pixel *pixels){
-	
-	calculatePixelsPosition(aSpace, pixels);
 
-	for(int i = 0; i < HEIGHT; i++){
-		for(int j = 0; j < WIDTH; j++){
-			pixels[i * WIDTH + j].bright = calculateEscapeVelocity(pixels[i * WIDTH + j].position, (*aSpace).constantC);
+
+void findJuliaSet (SetSpace *aSpace, Pixel *pixels, int width,int  height){
+
+	calculatePixelsPosition(aSpace, pixels, width, height);
+
+	for(int i = 0; i < height; i++){
+		for(int j = 0; j < width; j++){
+			pixels[i * width + j].bright = calculateEscapeVelocity(pixels[i * width + j].position, (*aSpace).constantC);
 		}
 	}
+}
+
+
+
+int main(int argc, char *argv[]){												//por ahora argv tiene el orden = 1. archivo 2. resolucion
+																				//3. offset 4. C 5. width 6. height
+	SetSpace* aSpace;
+	GraphicSettings* theGraphicSettings;
+	inputValidationAndInitialization(argc, argv, &aSpace, &theGraphicSettings);
+	findJuliaSet(aSpace, (*theGraphicSettings).pixelGrid,(*theGraphicSettings).widthResolution,(*theGraphicSettings).heightResolution);
+	printOutput(theGraphicSettings);
+
+	free(aSpace);
+	free(theGraphicSettings);
+
+	return 0;
 }
