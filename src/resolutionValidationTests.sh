427d2cf2870d702f0f371822f0f197d5d5686065
diegofk26 <diegofk26@gmail.com>
2016-09-04 17:26:32 -0300

    Tests para probar el paso del parametro resolution

--- src/resolutionValidationTests.sh
+++ src/resolutionValidationTests.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-gcc JuliaSet.c -o resValidation
+make
+
 echo -e  "ARGUMENTO: -r 300x200\n"
 echo "salida: "
-./resValidation -r 300x200
+./tp0 -r 300x200
 echo -e "\n"
 
 echo -e  "ARGUMENTO: -r 100x\n"
 echo "salida: "
-./resValidation -r 100x
+./tp0 -r 100x
 echo -e "\n"
 
 echo -e  "ARGUMENTO: -r \n"
 echo "salida: "
-./resValidation -r
+./tp0 -r
 
 echo -e "\n"
 
 echo -e  "ARGUMENTO: -r asdf\n"
 echo "salida: "
-./resValidation -r asdf
+./tp0 -r asdf
 echo -e "\n"
 
 echo -e  "ARGUMENTO: -r 10x1alskdj\n"
 echo "salida: "
-./resValidation -r 10x1alskdj
+./tp0 -r 10x1alskdj
 echo -e "\n"
