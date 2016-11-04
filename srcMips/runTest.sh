35ba16eee380f0c501a9c7e919feb740c58903bf
diegofk26 <diegofk26@gmail.com>
2016-11-03 23:11:55 -0300

    Version final

--- srcMips/runTest.sh
+++ srcMips/runTest.sh
@@ -1,13 +1,19 @@
-echo "Diferencias entre archivos utilizando parametros por defecto \n" 
+echo "Corriendo: ./tp1 -o std.pgm"
 
 ./tp1 -o "std.pgm"
+
+echo "Diferencias entre el archivo generado con el .S y el generado con el .c" 
 
 comm -3 "std.pgm" "images/std.pgm"
 
 
-echo "Diferencias entre archivos utilizando tp1 -C -1.125-0.21650635094611i -o dos.pgm \n"
+echo "Corriendo: ./tp1 -C -1.125-0.21650635094611i -o dos.pgm"
 
 
 ./tp1 -C -1.125-0.21650635094611i -o dos.pgm
+
+
+echo "Diferencias entre el archivo generado con el .S y el generado con el .c" 
+
 
 comm -3 "dos.pgm" "images/dos.pgm"
